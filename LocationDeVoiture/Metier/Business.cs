ceec46bae4fb2093d8b1c72a508fc782efe1af71
Ronny KWON <ronnykwon@gmail.com>
2020-10-28 10:50:48 -0930

    Add project files.

---
+++ LocationDeVoiture/Metier/Business.cs
@@ -0,0 +1,40 @@
+﻿using ServiceStack;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReparationPhone.Metier
+{
+    
+    public class Business
+    {
+        public static ObservableCollection<Reparation> Reparations { get; }
+
+        static Business()
+        {
+            Reparations = new ObservableCollection<Reparation>();
+            Reparation k = new Reparation("Kohu", "Bars", "87878787", "kohu@bars.com", "iPhone 9", "écran cassée au niveau de la caméra, bouton home ne fonctionne plus");
+            
+
+            //Business.Reparations.Add(new Reparation("KWON", "Ronny","87305445", "ronny@kwon.com", "Xiao Mi Mi 9", "écran fissuré"));
+            //Business.Reparations.Add(new Reparation("Ana", "Bak","87787980", "ana@bak.com", "Samsung J9", "Chargeur ne fonctionne plus"));
+        }
+
+        public static ObservableCollection<Reparation> GetByModel(string pattern)
+        {
+            ObservableCollection<Reparation> results = new ObservableCollection<Reparation>();
+            foreach(Reparation rep in Reparations)
+            {
+                if(rep.ModelPhone.Matches(pattern))
+                {
+                    results.Add(rep);
+                }
+            }
+
+            return results;
+        }
+    }
+}
---
+++ LocationDeVoiture/Metier/ReparationAccess.cs
@@ -0,0 +1,80 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReparationPhone.Metier
+{
+    
+    class ReparationAccess
+    {
+        private static MySqlConnection connection;
+
+        
+
+        static ReparationAccess()
+        {
+            String connString = "Server=localhost;Database=telephones;userid=rkwon;Pwd=";
+            connection = new MySqlConnection(connString);
+            //connection.Open();
+        }
+
+
+
+        public static void GetAllReparation()
+        {
+            
+            // on prepare la requête
+            String sql = "SELECT * FROM reparation";
+            using (MySqlCommand command = new MySqlCommand(sql, connection))
+            {
+                command.Connection.Open();
+                // On exécute la requête
+                using (DbDataReader dbReader = command.ExecuteReader())
+                {
+                    while (dbReader.Read())
+                    {
+                        string nom = dbReader.GetString(1);
+                        string prenom = dbReader.GetString(2);
+                        string contact = dbReader.GetString(4);
+                        string email = dbReader.GetString(3);
+                        string modelPhone = dbReader.GetString(5);
+                        string description = dbReader.GetString(8);
+                        Business.Reparations.Add(new Reparation(nom, prenom, contact, email, modelPhone, description));
+                    }
+                }
+
+                command.Connection.Close();
+            }
+        }
+
+        public static bool InsertReparation(Reparation r)
+        {
+            string sql = "INSERT INTO reparation(idReparation, Nom, Prenom, Email, Contact, ModelPhone, ReceivedDate, ExpectedFinishDate, Description, Prix) " +
+                "VALUES(@idReparation, @nom, @prenom, @email, @contact,@modelPhone, @receivedDate, @expectedFinishDate, @description, @prix)";
+            using (MySqlCommand cmd = new MySqlCommand(sql, connection))
+            {
+                cmd.Connection.Open();
+                cmd.Parameters.AddWithValue("@idReparation", null);
+                cmd.Parameters.AddWithValue("@nom", r.Nom);
+                cmd.Parameters.AddWithValue("@prenom", r.Prenom);
+                cmd.Parameters.AddWithValue("@email", r.Email);
+                cmd.Parameters.AddWithValue("@contact", r.Contact);
+                cmd.Parameters.AddWithValue("@modelPhone", r.ModelPhone);
+                cmd.Parameters.AddWithValue("@receivedDate", r.ReceivedDate);
+                cmd.Parameters.AddWithValue("@expectedFinishDate", r.ExpectedFinishDate);
+                cmd.Parameters.AddWithValue("@description", r.Description);
+                cmd.Parameters.AddWithValue("@prix", r.Prix);
+                
+                bool result = cmd.ExecuteNonQuery() == 1;
+                cmd.Connection.Close();
+                return result;
+            }
+
+        }
+    }
+}
