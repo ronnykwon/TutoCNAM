2eb927425479d84c50d5e85d129f37b4c3541efd
Ronny KWON <ronnykwon@gmail.com>
2020-10-29 11:12:31 -0930

    add new project for usercontroltest

---
+++ UserControlTest/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UserControlTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void YellowButton_Click(object sender, RoutedEventArgs e)
+        {
+            Yellow yellow = new Yellow();
+            this.UserControlHolder.Children.Clear();
+            this.UserControlHolder.Children.Add(yellow);
+        }
+
+        private void Red_Click(object sender, RoutedEventArgs e)
+        {
+            Red red = new Red();
+            this.UserControlHolder.Children.Clear();
+            this.UserControlHolder.Children.Add(red);
+        }
+    }
+}
